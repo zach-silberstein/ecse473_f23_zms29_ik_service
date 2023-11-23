d726f258b2ced9b87f5f11e3196d1e8f791904dc
Zach Silberstein <zms29@case.edu>
2023-11-23 13:55:09 -0500

    Fixed bugs in nodes so they now work

--- src/ik_client.cpp
+++ src/ik_client.cpp
@@ -1,29 +1,31 @@
 #include "ros/ros.h"
 #include "ik_service/PoseIK.h"
 #include <cstdlib>
+#include "geometry_msgs/Pose.h"
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "add_two_ints_client");
-  if (argc != 3)
-  {
-    ROS_INFO("usage: add_two_ints_client X Y");
-    return 1;
-  }
+  ros::init(argc, argv, "pose_ik_client");
+  //if (argc != 3)
+  //{
+  //  ROS_INFO("usage: add_two_ints_client X Y");
+  //  return 1;
+  //}
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<ik_service::PoseIK>("pose_ik");
-  //beginner_tutorials::AddTwoInts srv;
-  //srv.request.a = atoll(argv[1]);
-  //srv.request.b = atoll(argv[2]);
-  //if (client.call(srv))
-  //{
-  //  ROS_INFO("Sum: %ld", (long int)srv.response.sum);
-  //}
-  //else
-  //{
-   // ROS_ERROR("Failed to call service add_two_ints");
-    //return 1;
-  //}
+  ik_service::PoseIK ik_pose;
+  geometry_msgs::Pose part_pose;
+
+  part_pose.position.x = 0.5;
+  ik_pose.request.part_pose = part_pose;
+
+  if (client.call(ik_pose)) {
+    ROS_INFO("Call to ik_service returned [%i] solutions", ik_pose.response.num_sols);
+  }
+  else {
+    ROS_ERROR("Failed to call service ik_service");
+    return 1;
+  }
 
   return 0;
 }
--- src/ik_service.cpp
+++ src/ik_service.cpp
@@ -5,16 +5,16 @@ bool pose_ik(ik_service::PoseIK::Request  &req,
          ik_service::PoseIK::Response &res)
 {
   ROS_INFO("Pose Information...");
+  res.num_sols = -1;
   return true;
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "add_two_ints_server");
+  ros::init(argc, argv, "pose_ik_server");
   ros::NodeHandle n;
 
   ros::ServiceServer service = n.advertiseService("pose_ik", pose_ik);
-  ROS_INFO("Ready to add two ints.");
   ros::spin();
 
   return 0;
