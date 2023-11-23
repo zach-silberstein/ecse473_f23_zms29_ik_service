202837d0627b548aaa1a2e8fe38a58e457f188f1
Zach Silberstein <zms29@case.edu>
2023-11-23 12:58:14 -0500

    Updated client so that it would make

--- src/ik_client.cpp
+++ src/ik_client.cpp
@@ -12,18 +12,18 @@ int main(int argc, char **argv)
   }
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<ik_service::PoseIK>("pose_ik");
-  beginner_tutorials::AddTwoInts srv;
-  srv.request.a = atoll(argv[1]);
-  srv.request.b = atoll(argv[2]);
-  if (client.call(srv))
-  {
-    ROS_INFO("Sum: %ld", (long int)srv.response.sum);
-  }
-  else
-  {
-    ROS_ERROR("Failed to call service add_two_ints");
-    return 1;
-  }
+  //beginner_tutorials::AddTwoInts srv;
+  //srv.request.a = atoll(argv[1]);
+  //srv.request.b = atoll(argv[2]);
+  //if (client.call(srv))
+  //{
+  //  ROS_INFO("Sum: %ld", (long int)srv.response.sum);
+  //}
+  //else
+  //{
+   // ROS_ERROR("Failed to call service add_two_ints");
+    //return 1;
+  //}
 
   return 0;
 }
